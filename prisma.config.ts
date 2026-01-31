4c93ae67fb4cdcf2c4f0a5e9b8226d60703e524f
Stylz91 <fbaier91@gmail.com>
2026-01-31 10:05:59 +0100

    try 31012026 to run puppeteer

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'prisma/config'
 // Safety check: Verify the variable exists
 if (!process.env.DATABASE_URL) {
   console.error("❌ ERROR: DATABASE_URL is missing in prisma.config.ts!");
-  // If we are in production, we crash the app so we know something is wrong
+  // If we are in production, we crash the app so we know something is wrong.
   if (process.env.NODE_ENV === 'production') {
      throw new Error("DATABASE_URL is not set.");
   }
